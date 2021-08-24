874f7c3befcd627a060cc84c20a0dd92db85886f
kenta suhara <ponta.blockchain@gmail.com>
2021-08-24 15:12:49 +0900

    fix: firebase deploy関係

--- packages/react-app/tailwind.config.js
+++ packages/react-app/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/components/**/*.tsx"],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}',"./src/components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
